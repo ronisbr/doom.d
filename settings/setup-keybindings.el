9bb182aebba4245d894fea31539c4420fcb49696
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2020-12-29 08:26:41 +0530

    :sparkles: Add option to open Org directory

--- settings/setup-keybindings.el
+++ settings/setup-keybindings.el
@@ -23,7 +23,9 @@
       (:prefix ("l" . "fill")
        :desc "Copy character"   "c" #'ronisbr/copy-to-end
        :desc "Fill with char."  "f" #'ronisbr/fill-to-end
-       :desc "Fill entire line" "l" #'ronisbr/fill-line))
+       :desc "Fill entire line" "l" #'ronisbr/fill-line)
+      (:prefix ("o" . "open")
+       :desc "Org directory" "o" (lambda () (interactive) (dired ronisbr/org-directory))))
 
 (map! :nv "<down>" #'evil-next-visual-line
       :nv "<up>"   #'evil-previous-visual-line)
