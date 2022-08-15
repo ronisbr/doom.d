7fc22b8a83f7e4ba0bdc2acf56ccab4cf0c77c66
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-08-15 10:24:05 -0300

    :wrench: Highlight numbers with _ in Julia mode

--- settings/setup-julia-mode.el
+++ settings/setup-julia-mode.el
@@ -5,4 +5,14 @@
 ;; Automatically indent the code when typing `end`.
 (set-electric! 'julia-mode :words '("end"))
 
+;; In Julia, long numbers can be represented with an underscode.
+(after! julia-mode
+  (puthash 'julia-mode
+           (rx (and symbol-start
+                    (? "-")
+                    (+ digit)
+                    (0+ (and "_" (= 3 digit)))
+                    symbol-end))
+           highlight-numbers-modelist))
+
 (provide 'setup-julia-mode)
