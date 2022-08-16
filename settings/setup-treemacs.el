e6013af522bd9a781afed84c473e0c1942617463
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-08-16 15:10:56 -0300

    :bug: Add missing file

---
+++ settings/setup-treemacs.el
@@ -0,0 +1,8 @@
+;;; ~/.doom.d/settings/setup-treemacs.el -*- lexical-binding: t; -*-
+;;
+;; Configurations for Treemacs.
+
+(setq treemacs-follow-after-init t)
+(setq treemacs-use-follow-mode t)
+
+(provide 'setup-treemacs)
