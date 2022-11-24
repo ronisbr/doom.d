5dc877da1a0dec32b16816be9122886c337a164a
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-11-24 10:58:10 -0300

    :wrench: Update AUCTex configuration

--- settings/setup-auctex.el
+++ settings/setup-auctex.el
@@ -6,6 +6,18 @@
                        (getenv "PATH")))
 (add-to-list 'exec-path "/Library/TeX/texbin")
 
+(after! tex
+  (setq TeX-PDF-mode t)
+  (setq TeX-source-correlate-method (quote synctex))
+  (setq TeX-view-program-list
+        '(("Preview.app" "open -a Preview.app %o")
+          ("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")
+          ("open" "open %o"))
+        TeX-view-program-selection
+        '((output-dvi "open")
+          (output-pdf "Skim")
+          (output-html "open"))))
+
 (add-hook! LaTeX-mode (setq-local display-line-numbers nil))
 
 (provide 'setup-auctex)
