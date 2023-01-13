5a9a958db01d16d9e40660cfa06ef3abfa8113bc
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-01-13 11:26:03 -0300

    :art: Update theme and font

--- config.el
+++ config.el
@@ -3,12 +3,12 @@
 (setq user-full-name "Ronan Arraes Jardim Chagas"
       user-mail-address "ronisbr@gmail.com")
 
-(setq doom-font (font-spec :family "SauceCodePro Nerd Font" :size 14))
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 14))
 
 ;; For some reason, when using emacs-plus, the greek fonts changes for lines
 ;; that are not selected when starting Julia mode.
 (add-hook! 'after-setting-font-hook
-  (set-fontset-font t 'greek "SauceCodePro Nerd Font"))
+  (set-fontset-font t 'greek "JetBrainsMono Nerd Font"))
 
 ;; Select locale.
 (setenv "LANG" "pt_BR.UTF-8")
@@ -16,7 +16,11 @@
 (setenv "LC_ALL" "pt_BR.UTF-8")
 (set-locale-environment "pt_BR.UTF-8")
 
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-monokai-classic)
+
+;; The theme `doom-monokai-classic' does not set the color for variables.
+(custom-theme-set-faces! '(doom-monokai-classic)
+  `(font-lock-variable-name-face :foreground ,(doom-lighten "#9C91E4" 0.45)))
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
