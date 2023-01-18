2e1a62c4ee5883cbae870635331c69050efe6070
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2023-01-18 17:07:47 -0300

    :wrench: Add advise for "J" to avoid cursor movement

--- settings/setup-evil.el
+++ settings/setup-evil.el
@@ -5,4 +5,33 @@
 ;; The scope of evil-snipe should be the entire visible buffer.
 (setq evil-snipe-scope 'whole-visible)
 
+;; =============================================================================
+;;                               Evil operators
+;; =============================================================================
+
+(after! evil
+  (evil-define-operator ronisbr/evil-join (beg end)
+    "Join the selected lines without moving the cursor.
+
+This advise was obtained from Doom Emacs but adapter to avoid moving the
+cursor."
+    :motion evil-line
+    :move-point nil
+    (save-excursion
+      (let* ((count (count-lines beg end))
+             (count (if (> count 1) (1- count) count))
+             (fixup-mark (make-marker)))
+        (dotimes (var count)
+          (if (and (bolp) (eolp))
+              (join-line 1)
+            (let* ((end (line-beginning-position 3))
+                   (fill-column (1+ (- end beg))))
+              (set-marker fixup-mark (line-end-position))
+              (fill-region-as-paragraph beg end nil t)
+              (goto-char fixup-mark)
+              (fixup-whitespace))))
+        (set-marker fixup-mark nil))))
+
+  (advice-add #'evil-join :override #'ronisbr/evil-join))
+
 (provide 'setup-evil)
