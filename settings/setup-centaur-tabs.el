b0fb6f5dec39952575d4a75993b06ce3365c8b58
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-01-19 18:52:07 -0300

    :wrench: Improve hook

--- settings/setup-centaur-tabs.el
+++ settings/setup-centaur-tabs.el
@@ -21,7 +21,6 @@
 (if (daemonp)
     (add-hook 'after-make-frame-functions
               (lambda (frame)
-                (with-selected-frame frame (ronisbr/fix-centaur-tabs)))
-              (ronisbr/fix-centaur-tabs)))
+                (with-selected-frame frame (ronisbr/fix-centaur-tabs)))))
 
 (provide 'setup-centaur-tabs)
