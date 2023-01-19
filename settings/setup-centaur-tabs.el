ca65ad6bb949de8ab487b17b7225bd1fc08fd2ed
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-01-19 18:42:37 -0300

    :wrench: Fix centaur-tabs when using emacsclient

--- settings/setup-centaur-tabs.el
+++ settings/setup-centaur-tabs.el
@@ -6,4 +6,22 @@
   (map! "C-S-<right>" #'centaur-tabs-forward)
   (map! "C-S-<left>" #'centaur-tabs-backward))
 
+;; Fix centaur-tabs when using emacsclient.
+;;    See: https://github.com/ema2159/centaur-tabs/issues/127
+;;         https://github.com/doomemacs/doomemacs/issues/6647
+
+(after! centaur-tabs
+  (setq centaur-tabs-set-bar 'right))
+
+(defun ronisbr/fix-centaur-tabs ()
+  (centaur-tabs-mode -1)
+  (centaur-tabs-mode)
+  (centaur-tabs-headline-match))
+
+(if (daemonp)
+    (add-hook 'after-make-frame-functions
+              (lambda (frame)
+                (with-selected-frame frame (ronisbr/fix-centaur-tabs)))
+              (ronisbr/fix-centaur-tabs)))
+
 (provide 'setup-centaur-tabs)
