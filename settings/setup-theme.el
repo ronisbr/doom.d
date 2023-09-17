bd5fbaf32ff41ae7850c4e0f88a9fd1756eb1951
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-09-17 11:02:30 -0300

    :bug: Fix Apple emojis in newest Doom version

--- settings/setup-theme.el
+++ settings/setup-theme.el
@@ -9,6 +9,7 @@
 ;; selected when starting Julia mode.
 (add-hook! 'after-setting-font-hook
   (set-fontset-font t 'greek "JetBrains Mono NL")
+  (set-fontset-font t 'emoji "Apple Color Emoji")
   ;; Some emojis we do not want to use Apple's font.
   (set-fontset-font t ?🅰 "Noto Sans Mono SemiCondensed")
   (set-fontset-font t ?🅱 "Noto Sans Mono SemiCondensed"))
